eebf6351c34ae6a78ea7c7001ecf3969057b0b6b
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-08-27 05:19:05 +0300

    chore(frontend): add ESLint configuration for production checks

---
+++ aegis-platform/frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-refresh/only-export-components': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+}
