53fba9fafc06c085a8b19a4cee6e4fd408081a53
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-08-29 01:45:27 +0300

    feat(security-sessions): add django app config

---
+++ packages/backend/django_project/security_sessions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SecuritySessionsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "security_sessions"
+    verbose_name = "Security Test Sessions"
