373918c8d6f9f7017f74640b8ad77dccb402adc5
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-08-29 21:17:36 +0300

    fix(django): use fully-qualified core app path

--- aegis-platform/backend/django_project/core/apps.py
+++ aegis-platform/backend/django_project/core/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'django_project.core'
+    label = 'core'
     verbose_name = 'Core'
