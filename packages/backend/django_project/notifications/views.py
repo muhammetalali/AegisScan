6a6dd3777453088326e1cf3458d097ce1855628a
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-08-31 18:44:49 +0300

    fix(notifications): use UUID type for project relation

--- packages/backend/django_project/notifications/views.py
+++ packages/backend/django_project/notifications/views.py
@@ -8,7 +8,7 @@ from .models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
-    project_id = serializers.IntegerField(read_only=True)
+    project_id = serializers.UUIDField(read_only=True)
 
     class Meta:
         model = Notification
