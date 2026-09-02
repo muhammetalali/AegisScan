2f50b405c353b922d3af6a71c65a6eaf0acb0262
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-09-02 23:59:57 +0300

    refactor(frontend): isolate runtime to authentication surface

--- aegis-platform/frontend/src/main.tsx
+++ aegis-platform/frontend/src/main.tsx
@@ -1,34 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
-import { Toaster as HotToast } from 'react-hot-toast'
 import App from './App'
 import './index.css'
-import { initAuth } from './stores/authStore'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 60 * 24,
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-})
-
-void initAuth()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <Toaster position="top-right" theme="system" />
-        <HotToast position="top-right" />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <App />
+    <Toaster position="top-right" theme="system" />
   </React.StrictMode>,
 )
