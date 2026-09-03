97c218dca65c62a7009afba80804900e74fbf73a
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-09-04 00:55:56 +0300

    Enable enterprise audit middleware

--- aegis-platform/backend/django_project/settings.py
+++ aegis-platform/backend/django_project/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 'django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware', 'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware', 'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware', 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware', 'django.contrib.auth.middleware.AuthenticationMiddleware', 'django_project.audit.middleware.EnterpriseAuditMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware', 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 ROOT_URLCONF='django_project.urls'; TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
 WSGI_APPLICATION='django_project.wsgi.application'; ASGI_APPLICATION='django_project.asgi.application'; DATABASES={'default':env.db('DATABASE_URL')}; DATABASES['default']['CONN_MAX_AGE']=60; DATABASES['default']['OPTIONS']={'connect_timeout':10}
