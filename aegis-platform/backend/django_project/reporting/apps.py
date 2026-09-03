a946b553cb5f80f9875d0dc83d34881fed379f2d
muhammetalali199 <58662420+muhammetalali@users.noreply.github.com>
2026-09-03 23:12:51 +0300

    feat(reporting): add reporting app config

---
+++ aegis-platform/backend/django_project/reporting/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ReportingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_project.reporting'
+    verbose_name = 'Security Reporting'
